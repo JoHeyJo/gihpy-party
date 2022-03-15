f9e53073501adbdedbc2b1dd9fa2de3ea85865b6
jf <jpf0628@gmail.com>
2022-03-15 16:25:58 -0700

    add html and js files. Adding API key

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+"use strict"
+
+async function getGif() {
+  let searchInput = $("#search-input").val();
+
+  let response = await axios.get(
+    "http://api.giphy.com/v1/gifs/search", { params: { searchInput }}
+  );
+
+  console.log('resp.... ' + response);
+}
+
 console.log("Let's get this party started!");
